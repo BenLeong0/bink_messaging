7bf33e85f4dab61aa25efc04e64deec58cc7b365
= <benleong0@gmail.com>
2021-07-17 18:31:18 +0100

    socketio

--- server/main.py
+++ server/main.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_socketio import SocketIO, send
 
 app = Flask('bink')
+app.config['SECRET_KEY'] = 'a pithy aphorism'
+socketio = SocketIO(app)
 
 
-@app.route("/")
-def homepage():
-    print('yo')
-    return 'yo'
+@socketio.on("message")
+def handle_message(msg):
+    print('Message:', msg)
+    send(msg, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
