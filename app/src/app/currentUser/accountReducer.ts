2090eea00284c18d328911f3e27e98d8c0e7a360
= <benleong0@gmail.com>
2021-07-18 15:12:53 +0100

    account action types enum

--- app/src/app/currentUser/accountReducer.ts
+++ app/src/app/currentUser/accountReducer.ts
@@ -1,5 +1,7 @@
 import Account from "../../Types/Account";
 
+import ACCOUNT_ACTION_TYPES from "./accountActionTypes"
+
 interface AccountAction {
     type: string;
     payload: Account;
@@ -7,9 +9,9 @@ interface AccountAction {
 
 export default function accountReducer(state: Account=new Account(), action: AccountAction) {
     switch (action.type) {
-        case ("login"):
+        case (ACCOUNT_ACTION_TYPES.LOGIN):
             return action.payload;
-        case ("logout"):
+        case (ACCOUNT_ACTION_TYPES.LOGOUT):
             return new Account();
         default:
             return new Account();
