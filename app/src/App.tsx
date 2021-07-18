1656999a7be4b34b3d87cc682f09f2aa7202773b
benleong0 <benleong@urbanjungle.com>
2021-07-18 16:30:05 +0100

    ignore type lint

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import ChatBox from './components/ChatBox/ChatBox';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
-  return (
-    <div className="App">
-      <ChatBox />
-    </div>
-  );
+    return (
+        <div className="App">
+            <ChatBox />
+        </div>
+    );
 }
 
 export default App;
