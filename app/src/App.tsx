036f839bf9eef0ca3f147c024b7cb3f20da9d111
= <benleong0@gmail.com>
2021-07-19 23:54:18 +0100

    revert app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,29 +1,14 @@
-import React, { useEffect } from 'react';
-import socketIOClient from "socket.io-client";
+import React from 'react';
 import './App.css';
-
-import store from './app/store';
-import { connect } from './app/socket/socketActionCreators';
-
-import MessageService from './components/core/MessageService';
 import ChatBox from './components/ChatBox/ChatBox';
 
-
-const App: React.FC = () => {
-    const messageService = new MessageService();
-
-    useEffect(() => {
-        const socket = socketIOClient(messageService.API_URL);
-        store.dispatch(connect(socket));
-        socket.on("connect", () => messageService.sendMessage("User has connected!"));
-        socket.on("message", messageService.appendMessage);
-    }, []);
-
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function App() {
     return (
         <div className="App">
             <ChatBox />
         </div>
     );
-};
+}
 
 export default App;
