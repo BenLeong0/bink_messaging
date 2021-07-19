1da7d4d15efde9bacd03b8376abcaaeda20c3cf2
= <benleong0@gmail.com>
2021-07-19 23:52:12 +0100

    initalise store on launch

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import socketIOClient from "socket.io-client";
 import './App.css';
+
+import store from './app/store';
+import { connect } from './app/socket/socketActionCreators';
+
+import MessageService from './components/core/MessageService';
 import ChatBox from './components/ChatBox/ChatBox';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-function App() {
+
+const App: React.FC = () => {
+    const messageService = new MessageService();
+
+    useEffect(() => {
+        const socket = socketIOClient(messageService.API_URL);
+        store.dispatch(connect(socket));
+        socket.on("connect", () => messageService.sendMessage("User has connected!"));
+        socket.on("message", messageService.appendMessage);
+    }, []);
+
     return (
         <div className="App">
             <ChatBox />
         </div>
     );
-}
+};
 
 export default App;
