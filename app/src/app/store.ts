20622298446f2c2d9cacdae5b213c1f3d27475b4
= <benleong0@gmail.com>
2021-07-19 22:59:47 +0100

    combined reducer

--- app/src/app/store.ts
+++ app/src/app/store.ts
@@ -1,6 +1,16 @@
-import { createStore } from "redux";
-import accountReducer from "./currentUser/accountReducer";
+import { combineReducers, createStore } from "redux";
 
-const store = createStore(accountReducer);
+import accountReducer from "./currentUser/accountReducer";
+import messagesReducer from "./messages/messagesReducer";
+import socketReducer from "./socket/socketReducer";
+
+
+const reducer = combineReducers({
+    account: accountReducer,
+    messages: messagesReducer,
+    socket: socketReducer
+});
+
+const store = createStore(reducer);
 
 export default store;
