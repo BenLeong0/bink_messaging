556edd09ee3be3b640b323b326c3adb0740b3870
= <benleong0@gmail.com>
2021-07-19 22:35:27 +0100

    reducer

---
+++ app/src/app/socket/socketReducer.ts
@@ -0,0 +1,15 @@
+import { SocketAction } from "./socketActionCreators";
+import { Socket } from "socket.io-client";
+
+import * as SOCKET_ACTIONS from "./socketActionTypes";
+
+export default function socketReducer(state: Socket, action: SocketAction): Socket | undefined {
+    switch (action.type) {
+    case (SOCKET_ACTIONS.CONNECT):
+        return action.payload;
+    case (SOCKET_ACTIONS.DISCONNECT):
+        return undefined;
+    default:
+        return state;
+    }
+}
