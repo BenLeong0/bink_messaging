1f3e0e513ca4cfea9cba0d2fa03ed5af139870b0
= <benleong0@gmail.com>
2021-07-19 22:35:08 +0100

    action creators

---
+++ app/src/app/socket/socketActionCreators.ts
@@ -0,0 +1,21 @@
+import { Socket } from "socket.io-client";
+import * as SOCKET_ACTIONS from "./socketActionTypes";
+
+export interface SocketAction {
+    type: string;
+    payload: Socket | undefined
+}
+
+export function connect(socket: Socket): SocketAction {
+    return {
+        type: SOCKET_ACTIONS.CONNECT,
+        payload: socket
+    };
+}
+
+export function disconnect(): SocketAction {
+    return {
+        type: SOCKET_ACTIONS.DISCONNECT,
+        payload: undefined
+    };
+}
