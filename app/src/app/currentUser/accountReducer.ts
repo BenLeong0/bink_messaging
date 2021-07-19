6e0ca058cbcdf726a68eda77646183d55afd96a9
= <benleong0@gmail.com>
2021-07-19 23:08:43 +0100

    spacing

--- app/src/app/currentUser/accountReducer.ts
+++ app/src/app/currentUser/accountReducer.ts
@@ -1,6 +1,6 @@
 import Account from "../../Types/Account";
-import { AccountAction } from "./accountActionCreators";
 
+import { AccountAction } from "./accountActionCreators";
 import * as ACCOUNT_ACTIONS from "./accountActionTypes";
 
 export default function accountReducer(
