cf9937454ee9cb114f977d4b4781350d53f4b99f
= <benleong0@gmail.com>
2021-07-20 07:23:35 +0100

    send stringified jsons

--- app/src/components/core/MessageService.ts
+++ app/src/components/core/MessageService.ts
@@ -3,18 +3,33 @@ import { Socket } from "socket.io-client";
 import Message from "../../Types/Message";
 import { append } from "../../app/messages/messageActionCreators";
 
+import CoreService from "./CoreService";
+
 import store from "../../app/store";
 
 export default class MessageService {
 
     API_URL: string;
+    coreService: CoreService;
 
     constructor() {
         this.API_URL = process.env.NODE_ENV === "production" ? "127.0.0.1:5000" : "localhost:5000";
+        this.coreService = new CoreService();
     }
+
 
     getSocket(): Socket | null {
         return store.getState().socket;
+    }
+
+
+    buildMessage(message: string): Message {
+        const user = store.getState().account;
+        const username = user === null ? '' : user.username;
+
+        const timestamp = this.coreService.getCurrentTimestamp();
+
+        return new Message(message, username, timestamp);
     }
 
 
@@ -26,7 +41,8 @@ export default class MessageService {
             return;
         }
 
-        socket.send(message);
+        const msgObj = this.buildMessage(message);
+        socket.send(JSON.stringify(msgObj));
     }
 
 
